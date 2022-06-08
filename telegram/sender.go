d65fdc1cc922757dbf57134baf8414b533d21964
0xMett <mattiafailla@hotmail.it>
2022-06-08 22:03:33 +0000

    Load telegram token from env

--- telegram/sender.go
+++ telegram/sender.go
@@ -1,14 +1,16 @@
 package telegram
 
 import (
+	"fmt"
+	"os"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 // Telegram configs
-const telegram_token string = ""
-var bot, _ = tgbotapi.NewBotAPI(telegram_token)
+var bot, _ = tgbotapi.NewBotAPI(os.Getenv("TelegramBotToken"))
 
 func SendMessage(message string) {
+	fmt.Println("TOKEN:" ,os.Getenv("TelegramBotToken"))
 	msg := tgbotapi.NewMessage(-1001601201441, message)
 	bot.Send(msg)
 }
